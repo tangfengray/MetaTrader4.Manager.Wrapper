018552847bf79e53a9cdb9a61fcff345b546b883
Nikita Gusev <nikita.gusev.1987@gmail.com>
2015-06-13 12:57:50 +0200

    Total property marked as obsolete in DailyGroupRequest and ReportGroupRequest

--- src/Projects/P23.MetaTrader4.Manager.Contracts/DailyGroupRequest.cs
+++ src/Projects/P23.MetaTrader4.Manager.Contracts/DailyGroupRequest.cs
@@ -11,5 +11,8 @@ namespace P23.MetaTrader4.Manager.Contracts
         public string Name { get; set; }            // group name
         public UInt32 From { get; set; }            // from
         public UInt32 To { get; set; }              // to
+        
+        [Obsolete]
+        public int Total { get; set; }              // total logins in request group
     }
 }
