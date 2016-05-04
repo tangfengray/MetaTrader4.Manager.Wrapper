2c624e83b0133e576bf7f022cad3282addd02a11
Nikita Gusev <nikita.gusev.1987@gmail.com>
2016-05-04 21:10:03 +0200

    Added TradesUserHistory test

---
+++ src/P23.MetaTrader4.Manager.Tests/TradesUserHistoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P23.MetaTrader4.Manager.Tests.Helpers;
+
+namespace P23.MetaTrader4.Manager.Tests
+{
+    [TestClass]
+    public class TradesUserHistoryTests
+    {
+        [TestMethod]
+        public void TradesUserHistoryTest1()
+        {
+            using (var mt = TestHelpers.CreateWrapper())
+            {
+                //Act
+                var @from = new DateTimeOffset(2016, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
+                var to = @from.AddYears(1);
+                var trades = mt.TradesUserHistory(1000, 0, to);
+                
+                //Assert
+                Assert.IsTrue(trades.Count > 0);
+            }
+        }
+    }
+}
