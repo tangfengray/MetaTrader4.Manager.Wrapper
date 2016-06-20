1f466c5cc9257a4b7a07066c843e12bc23ccce4c
Nikita Gusev <Nikita Gusev>
2016-06-20 20:50:41 +0200

    Fixed assembly signing

--- src/Scripts/versionUpdater.ps1
+++ src/Scripts/versionUpdater.ps1
@@ -12,3 +12,9 @@ $resourceFile = $resourceFile -replace "\d+,\d+,\d+,\d+", $fileVer
 $resourceFile = $resourceFile -replace "\d+\.\d+\.\d+\.\d+", $newVersion
 $resourceFile = $resourceFile.Trim()
 Out-File -FilePath $fileName -InputObject $resourceFile
+
+$assemblyInfoFileName = [IO.Path]::Combine($solutionDirectory, "P23.MetaTrader4.Manager.ClrWrapper\AssemblyInfo.cpp")
+$assemblyInfoFile = Get-Content $assemblyInfoFileName -Raw
+$assemblyInfoFile = $assemblyInfoFile -replace "AssemblyVersionAttribute[(]"".+""", [String]::Format("AssemblyVersionAttribute(""{0}""", $newVersion)
+$assemblyInfoFile = $assemblyInfoFile.Trim()
+Out-File -FilePath $assemblyInfoFileName -InputObject $assemblyInfoFile
